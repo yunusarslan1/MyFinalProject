3b99904626f9a206380be47d089456bd3f0eeaa8
leonyunus <58307367+yunusarslan1@users.noreply.github.com>
2021-03-27 16:54:48 +0300

    I made an E commerce site with C # core...

---
+++ Business/Constans/Messages.cs
@@ -0,0 +1,29 @@
+﻿using Core.Entities.Concrete;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace Business.Constans
+{
+    public static class Messages
+    {
+        public static string ProductAdded = "Ürün eklendi";
+        public static string ProductNameInvalid = "Ürün ismi geçersiz";
+        public static string MaintenanceTime = "Sistem bakımda";
+        public static string ProductsListed = "Ürünler Listelendi";
+        public static string ProductCountOfCategoryError = "Bir kategorıde en fazla 10 ürün olmalıdır";
+        public static string ProductNameAlreadyExists = "Bu isimde zaten başka ürün var";
+        public static string CategoryLimitExceded="Kategory limiti aşıldığı için yeni ürün eklenemiyor...";
+        public static string AuthorizationDenied="Yetkiniz yok.";
+
+        // AuthManager
+        public static string UserRegistered = "Kayıt Olundu...";
+        public static string UserNotFound="Kullanıcı bulunamadı";
+        public static string PasswordError="Parola hatası";
+        public static string SuccessfulLogin="Login başarılı";
+        public static string UserAlreadyExists="Kullanıcı başarılı";
+        public static string AccessTokenCreated="Token oluşturuldu";
+    }
+}
---
+++ Core/Utilities/Security/JWT/AccessToken.cs
@@ -0,0 +1,18 @@
+﻿using Core.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Security.JWT
+{
+    public class AccessToken:ITokenHelper
+    {
+        public string Token { get; set; }
+        public DateTime Expiration { get; set; }
+
+        public AccessToken CreateToken(User user, List<OperationClaim> operationClaims)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
